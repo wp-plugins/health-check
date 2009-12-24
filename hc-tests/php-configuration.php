a1a28cfde2cd181b19fda404719ae682cae0bca8
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-24 18:52:53 +0000

    Write the first real test for the php.ini default_charset configuration item.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@186907 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hc-tests/php-configuration.php
+++ hc-tests/php-configuration.php
@@ -1,10 +1,30 @@
 <?php
-/*
- * Health Check 
+/**
+ * Tests to check for php config issues.
+ *
+ * @package HealthCheck
+ * @subpackage Tests
  */
-// http://blog.ftwr.co.uk/archives/2009/09/29/missing-dashboard-css-and-the-perils-of-smart-quotes/
-add_action('health_check_run_tests','health_check_php_default_charset');
-function health_check_php_default_charset() {
-	//TODO validate the default_charset option
+/**
+ * Check that default_charset is not set to a bad value in php.ini
+ * 
+ * Validates against the following rules:
+ * 
+ * 	Max 40 chars
+ * 	A-Z
+ *  
+ * @link http://www.w3.org/International/O-HTTP-charset
+ * @link http://www.iana.org/assignments/character-sets
+ * @link http://blog.ftwr.co.uk/archives/2009/09/29/missing-dashboard-css-and-the-perils-of-smart-quotes/
+ * @author peterwestwood
+ *
+ */
+class HealthCheck_PHP_DefaultCharset extends HealthCheckTest {
+	function run_test() {
+		$configured = ini_get('default_charset');
+		$filtered = preg_replace('|[^a-z0-9_.\-:]|i', '', $configured);
+		$this->assertEquals($configured, $filtered, sprintf( __( 'Default character set configured in php.ini %s contains illegal characters.', 'health-check' ), $configured), HEALTH_CHECK_ERROR );
+	}
 }
+HealthCheck::register_test('HealthCheck_PHP_DefaultCharset');
 ?>
