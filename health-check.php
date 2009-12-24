28ef505a86322e5ef0ebab2da2bbe5b57c8b7030
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-24 12:50:10 +0000

    Rework the admin page into a two step process and bring in the new includes / respect the new location for test files.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@186849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- health-check.php
+++ health-check.php
@@ -8,14 +8,14 @@
 	Author URI: http://wordpress.org/extend/plugins/health-check/
  */
 
-class health_check {
+class HealthCheck {
 	
 	function action_plugins_loaded() {
-		add_action('admin_menu', array('health_check', 'action_admin_menu'));
+		add_action('admin_menu', array('HealthCheck', 'action_admin_menu'));
 	}
 
 	function action_admin_menu() {
-		add_management_page(__('Health Check','health_check'), 'Health Check', 'manage_options', 'health_check', array('health_check','display_page'));
+		add_management_page(__('Health Check','health_check'), __('Health Check','health_check'), 'manage_options', 'health_check', array('HealthCheck','display_page'));
 	}
 
 	function display_page() {
@@ -23,26 +23,70 @@ class health_check {
 		{
 			wp_die( __('You do not have sufficient permissions to access this page.') );
 		}
-		//Lazy load all the tests we will run
-		health_check::load_tests();
+		
+		$step = HealthCheck::_fetch_array_key($_GET, 'step', 0);
+		
 ?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
 		<h2><?php _e('Health Check','health_check'); ?></h2>
 		<p><?php _e('Welcome to your WordPress health check centre.','health_check');?></p>
-		<p><?php _e('I am now going to run a number of tests on your site and report back on any issues I find.','health_check');?></p>
-		<?php do_action('health_check_run_tests'); ?>
+<?php
+		if (0 == $step) {
+?>
+		<p><?php _e('Click on go to run a number of tests on your site and report back on any issues.','health_check');?></p>
+		<p class="submit"><a type="submit" class="button-primary" href="<?php echo admin_url("tools.php?page=health_check&step=1");?>"><?php _e('Go','health_check') ?></a></p>
+		
+<?php
+		} else {
+			//Lazy load our includes and all the tests we will run
+			HealthCheck::load_includes();
+			HealthCheck::load_tests();
+			HealthCheck::run_tests();
+		}
+?>
 	</div>
 <?php
 	}
 	
+	function run_tests() {
+		//TODO
+	}
+	
+	function register_test() {
+		//TODO
+	}
+	/**
+	 * Load all the test classes we have.
+	 * 
+	 * Each test class must also be registered by calling HealthCheck::register_test()
+	 * 
+	 * @return none
+	 */
 	function load_tests() {
-		//TODO include the test files.
-		$tests_dir = plugin_dir_path(__FILE__) . 'tests/';
-		require_once($tests_dir . 'php-configuration.php');
+		$hc_tests_dir = plugin_dir_path(__FILE__) . 'hc-tests/';
+		require_once($hc_tests_dir . 'php-configuration.php');
+	}
+	
+	/**
+	 * Load in our include files.
+	 * 
+	 * @return none
+	 */
+	function load_includes() {
+		$hc_includes = plugin_dir_path(__FILE__) . 'hc-includes/';
+		require_once($hc_includes . 'class.health-check-test.php');
+		require_once($hc_includes . 'class.health-check-test-result.php');
+	}
+	
+	/**
+	 * Retrieves a value from an array by key without a notice
+	 */
+	function _fetch_array_key( $array, $key, $default = '' ) {
+		return isset( $array[$key] )? $array[$key] : $default;
 	}
 }
 /* Initialise outselves */
-add_action('plugins_loaded', array('health_check','action_plugins_loaded'));
+add_action('plugins_loaded', array('HealthCheck','action_plugins_loaded'));
 
 ?>
