dcae6b61006bc76e94d674785ee9b38427ac6ace
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-24 13:08:51 +0000

    Add a dummy test to test the framework with.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@186857 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hc-tests/dummy-test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Dummy test to check that everything is working.
+ *
+ * @package HealthCheck
+ * @subpackage Tests
+ */
+/**
+ * Dummy test to check that everything is working.
+ * @author peterwestwood
+ *
+ */
+class HealthCheck_DummyTest extends HealthCheckTest {
+	function run_test() {
+		$this->result->markAsFailed(__('Dummy Test Ran ok.','health_check'), HEALTH_CHECK_OK);
+	}
+}
+
+HealthCheck::register_test('HealthCheck_DummyTests');
+?>
