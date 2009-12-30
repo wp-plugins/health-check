e4fbbde90bf5afba322e93fd82bbd7fa271bed11
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-30 10:42:48 +0000

    Wrap some lines to make the tests easier to read.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@188100 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hc-tests/php-configuration.php
+++ hc-tests/php-configuration.php
@@ -22,7 +22,9 @@ class HealthCheck_PHP_DefaultCharset extends HealthCheckTest {
 	function run_test() {
 		$configured = ini_get('default_charset');
 		$filtered = preg_replace('|[^a-z0-9_.\-:]|i', '', $configured);
-		$this->assertEquals($configured, $filtered, sprintf( __( 'Default character set configured in php.ini %s contains illegal characters.', 'health-check' ), $configured), HEALTH_CHECK_ERROR );
+		$this->assertEquals(	$configured, $filtered,
+							sprintf( __( 'Default character set configured in php.ini %s contains illegal characters.', 'health-check' ), $configured),
+							HEALTH_CHECK_ERROR );
 	}
 }
 HealthCheck::register_test('HealthCheck_PHP_DefaultCharset');
@@ -36,7 +38,9 @@ HealthCheck::register_test('HealthCheck_PHP_DefaultCharset');
  */
 class HealthCheck_PHP_Version extends HealthCheckTest {
 	function run_test() {
-		$this->assertTrue(version_compare('5.0.0', PHP_VERSION, '<'), sprintf( __( 'Your Webserver is currently using PHP version %s, which is no longer recieving security updates and will no longer be supported by WordPress in an upcoming version', 'health-check' ), PHP_VERSION ), HEALTH_CHECK_RECOMMENDATION );
+		$this->assertTrue(	version_compare('5.0.0', PHP_VERSION, '<'),
+							sprintf( __( 'Your Webserver is currently using PHP version %s, which is no longer recieving security updates and will no longer be supported by WordPress in an future version', 'health-check' ), PHP_VERSION ),
+							HEALTH_CHECK_RECOMMENDATION );
 	}
 }
 HealthCheck::register_test('HealthCheck_PHP_Version');
