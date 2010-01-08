b59e84d332ac2970ceefbb0301948d18e6c01774
Denis-de-Bernardy <Denis-de-Bernardy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-08 13:55:13 +0000

    HC: check for run time memory limit override
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@191512 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hc-tests/php-configuration.php
+++ hc-tests/php-configuration.php
@@ -111,19 +111,41 @@ HealthCheck::register_test('HealthCheck_LongArrays');
 
 
 /**
- * Check that long arrays are turned off
+ * Check that there is enough memory
  * 
  * @author Denis de Bernardy
  */
 class HealthCheck_MemoryLimit extends HealthCheckTest {
 	function run_test() {
-		$message = sprintf( __( 'Your Webserver is running PHP with a low memory limit (%s). This can occasionally prevent WordPress from working. In particular during core upgrades, if you use a theme with lots of functionality, or if you enable multitudes of plugins. Depending on how your server is configured, running into this memory limit would reveal some kind of "Failed to allocate memory" error, or a completely blank screen. Please contact your host to have them increase the memory limit to 32M or more. (48M or even 64M might be needed if you enable many plugins.)', 'health-check' ), ini_get('memory_limit') );
+		$message = sprintf( __( 'Your Webserver is running PHP with a low memory limit (%s). This can occasionally prevent WordPress from working. In particular during core upgrades, if you use a theme with lots of functionality, or if you enable multitudes of plugins. Depending on how your server is configured, running into this memory limit would reveal some kind of "Failed to allocate memory" error, an incomplete screen, or a completely blank screen. Please contact your host to have them increase the memory limit to 32M or more. (48M or even 64M might be needed if you enable many plugins.)', 'health-check' ), ini_get('memory_limit') );
 		$this->assertTrue(	!ini_get('memory_limit') || ( intval(ini_get('memory_limit')) >= 32 ),
 							$message,
 							HEALTH_CHECK_RECOMMENDATION );
 	}
 }
 HealthCheck::register_test('HealthCheck_MemoryLimit');
+
+
+/**
+ * Check that the memory limit can be overridden
+ * 
+ * @author Denis de Bernardy
+ */
+class HealthCheck_MemoryLimitOverride extends HealthCheckTest {
+	function run_test() {
+		$original_limit = ini_get('memory_limit');
+		$test = 247;
+		if ( $test == intval($original) )
+			$test++;
+		@ini_set('memory_limit', "{$test}M");
+		$message = __( 'Your Webserver disallows PHP to increase the memory limit at run time. This can occasionally prevent WordPress from working. In particular during core upgrades, where WordPress tries to increase it to 256M in order to unzip core files. Depending on how your server is configured, running into this memory limit would reveal some kind of "Failed to allocate memory" error, an incomplete screen, or a completely blank screen. Please contact your host to have them fix this.', 'health-check' );
+		$this->assertTrue(	( intval(ini_get('memory_limit')) == $test ),
+							$message,
+							HEALTH_CHECK_RECOMMENDATION );
+		@ini_set('memory_limit', $original_limit); // restore original limit
+	}
+}
+HealthCheck::register_test('HealthCheck_MemoryLimitOverride');
 
 
 /**
