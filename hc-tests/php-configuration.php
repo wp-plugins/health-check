87b128764f8fdc2e5a2f1d600187043611791422
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-09 10:17:12 +0000

    Use test result information rather than effectively re-running the same assertion twice.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@191835 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hc-tests/php-configuration.php
+++ hc-tests/php-configuration.php
@@ -17,11 +17,11 @@
 class HealthCheck_PHP_Version extends HealthCheckTest {
 	function run_test() {
 		$message = sprintf( __( 'Your Webserver is running PHP version %1$s, but the latest version is %2$s. WordPress will no longer support PHP 4 in a future version because it is <a href="%3$s">no longer receiving security updates</a>. Please contact your host and have them upgrade PHP as soon as possible.', 'health-check' ), PHP_VERSION, HEALTH_CHECK_PHP_VERSION, 'http://www.php.net/archive/2007.php#2007-07-13-1' );
-		$this->assertTrue(	version_compare('5.0.0', PHP_VERSION, '<'),
-							$message,
-							HEALTH_CHECK_RECOMMENDATION );
+		$passed = $this->assertTrue(	version_compare('5.0.0', PHP_VERSION, '<'),
+										$message,
+										HEALTH_CHECK_RECOMMENDATION );
 
-		if ( version_compare('5.0.0', PHP_VERSION, '<') ) { // no point in raising this twice
+		if ( $passed ) { // no point in raising this twice
 			$message = sprintf( __( 'Your Webserver is running PHP version %1$s, but the latest version is %2$s. Please contact your host and have them upgrade PHP.', 'health-check' ), PHP_VERSION, HEALTH_CHECK_PHP_VERSION );
 			$this->assertTrue(	version_compare(HEALTH_CHECK_PHP_VERSION, PHP_VERSION, '<='),
 								$message,
