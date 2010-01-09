e6a8f8b62bcc3fb6b5f982d0cb4f80a9b8645cce
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-09 10:18:10 +0000

    Move the latest version definitions into the hc-includes folder.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/alpha@191836 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- versions.php
+++
@@ -1,6 +0,0 @@
-<?php
-// latest versions of server software, defined here for more convenient updates
-define('HEALTH_CHECK_APACHE_VERSION', '2.2.14');
-define('HEALTH_CHECK_PHP_VERSION', '5.3.1');
-define('HEALTH_CHECK_MYSQL_VERSION', '5.1.42');
-?>
