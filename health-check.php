874e32441796f3901bf4cf7fa174d216ace94481
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 22:00:03 +0000

    Rework to take better consideration of wp-content/db.php drop ins and only warn about them if they don't pass the version check. Most of them as mysql based from caching plugins.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/branches/simple@266102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- health-check.php
+++ health-check.php
@@ -3,7 +3,7 @@
 	Plugin Name: Health Check
 	Plugin URI: http://wordpress.org/extend/plugins/health-check/
 	Description: Checks the health of your WordPress install (and then deactivates itself)
-	Author: The Health Check Team
+	Author: The WordPress.org community
 	Version: 0.1
 	Author URI: http://wordpress.org/extend/plugins/health-check/
  */
@@ -19,15 +19,21 @@ class HealthCheck {
 	function action_admin_notice() {
 		global $wpdb;
 		$php_version_check = version_compare(HEALTH_CHECK_PHP_VERSION, PHP_VERSION, '<');
-		$mysql_version_check = version_compare(HEALTH_CHECK_MYSQL_VERSION, $wpdb->db_version(), '<') || file_exists( WP_CONTENT_DIR . '/db.php' );
+		$mysql_version_check = version_compare(HEALTH_CHECK_MYSQL_VERSION, $wpdb->db_version(), '<');
+		$db_dropin = file_exists( WP_CONTENT_DIR . '/db.php' );
+		
 		$message = "<div id='health-check-warning' class='updated'>";
 		if ( !$php_version_check ) 
 			$message .= "<p><strong>".__('Warning:', 'health-check')."</strong> ".sprintf(__('Your server is running PHP version %1$s. WordPress 3.2 will require PHP version %2$s.', 'health-check'), PHP_VERSION, HEALTH_CHECK_PHP_VERSION)."</p>";
-		if ( !$mysql_version_check )
-			$message .= "<p><strong>".__('Warning:', 'health-check')."</strong> ".sprintf(__('Your server is running mySQL version %1$s. WordPress 3.2 will require mySQL version %2$s', 'health-check'), $wpdb->db_version(), HEALTH_CHECK_MYSQL_VERSION)."</p>";
-		
-		if ( $php_version_check && $mysql_version_check)
-			$message .= "<p><strong>".__('Excellent:', 'health-check')."</strong> ".sprintf(__('Your server is running PHP version %1$s and mySQL version %2$s which will be great for WordPress 3.2 onward. ', 'health-check'), PHP_VERSION, HEALTH_CHECK_MYSQL_VERSION)."</p>";
+		if ( !$mysql_version_check ) {
+			$message .= "<p><strong>".__('Warning:', 'health-check')."</strong> ".sprintf(__('Your server is running MySQL version %1$s. WordPress 3.2 will require MySQL version %2$s', 'health-check'), $wpdb->db_version(), HEALTH_CHECK_MYSQL_VERSION)."</p>";
+			
+			if ( $db_dropin )
+				$message .= "<p><strong>".__('Note:', 'health-check')."</strong> ".__('You are using a <code>wp-content/db.php</code> drop-in which may not being using a MySQL database if so you can ignore this warning.', 'health-check')."</p>";
+		}
+
+		if ( $php_version_check && $mysql_version_check )
+			$message .= "<p><strong>".__('Excellent:', 'health-check')."</strong> ".sprintf(__('Your server is running PHP version %1$s and mySQL version %2$s which will be great for WordPress 3.2 onward. ', 'health-check'), PHP_VERSION, $wpdb->db_version())."</p>";
 		else
 			$message .= "<p>".__('Once your host has upgraded your server you can re-activate the plugin to check again.', 'health-check')."</p>";
 
